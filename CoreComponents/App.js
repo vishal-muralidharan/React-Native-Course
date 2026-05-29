ad80213e521fba1bbc420c46e5e253c783c2950e
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-29 15:39:12 +0530

    CoreComponents - Setup

---
+++ CoreComponents/App.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'plum' }}>
+      <View style={{ width: 200, height: 200, backgroundColor: 'lightblue' }} />
+    </View>
+  );
+}
