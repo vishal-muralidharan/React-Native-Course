c87ffb99f0e1d49383470d1f7818e6449259eb75
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-29 15:50:23 +0530

    Image tag - 2

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -4,7 +4,7 @@ const logoImg = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Image source={logoImg} />
+      <Image source={logoImg} style={{width: 300, height:300}} />
     </View>
   );
 }
