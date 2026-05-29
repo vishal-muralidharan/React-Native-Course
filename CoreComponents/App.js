8433e6d476274cbdd09d21c564cd1f6a65331dc9
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-29 16:25:32 +0530

    Button tag

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,26 +1,14 @@
-import { View, Text, Image, ScrollView } from 'react-native';
-const logoImg = require("./assets/adaptive-icon.png");
+import { View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <ScrollView>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text style={{ color: 'white', marginTop: 16 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec condimentum, nisl eu ultrices ultricies, nunc nisl
-          aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec 
-          condimentum, nisl eu ultrices ultricies, nunc nisl aliquam 
-          nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum, 
-          nisl eu ultrices ultricies, nunc nisl aliquam nunc, eget 
-          aliquam nisl nunc sit amet nisl. Donec condimentum, nisl 
-          eu ultrices ultricies, nunc nisl aliquam nunc, eget aliquam 
-          nisl nunc sit amet nisl. Donec condimentum, nisl eu ultrices 
-          ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet 
-          nisl. Donec condimentum, nisl eu ultrices ultricies, nunc 
-          nisl aliquam nunc, eget aliquam nisl nunc sit amet nisl.
-        </Text>
-      </ScrollView>
+      <Button
+        title="Press"
+        onPress={() => console.log("Button pressed")}
+        color="midnightblue"
+        disabled
+      />
     </View>
   );
 }
