f3abca4065a50cf1ed7aeb39976e56c918535f1c
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-29 15:51:00 +0530

    Image tag - 3

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
       <Image source={logoImg} style={{width: 300, height:300}} />
+      <Image source={{uri:"https://picsum.photos/300"}} />
     </View>
   );
 }
