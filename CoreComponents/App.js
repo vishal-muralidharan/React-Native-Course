d3ac610e1e3050f81dada665f132d0ac9ff19108
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-29 16:00:57 +0530

    ScrollView tag

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,12 +1,26 @@
-import { View, Text, ImageBackground } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text style={{ color: 'white', fontSize: 18 }}>IMAGE TEXT</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text style={{ color: 'white', marginTop: 16 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Donec condimentum, nisl eu ultrices ultricies, nunc nisl
+          aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec 
+          condimentum, nisl eu ultrices ultricies, nunc nisl aliquam 
+          nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum, 
+          nisl eu ultrices ultricies, nunc nisl aliquam nunc, eget 
+          aliquam nisl nunc sit amet nisl. Donec condimentum, nisl 
+          eu ultrices ultricies, nunc nisl aliquam nunc, eget aliquam 
+          nisl nunc sit amet nisl. Donec condimentum, nisl eu ultrices 
+          ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet 
+          nisl. Donec condimentum, nisl eu ultrices ultricies, nunc 
+          nisl aliquam nunc, eget aliquam nisl nunc sit amet nisl.
+        </Text>
+      </ScrollView>
     </View>
   );
 }
