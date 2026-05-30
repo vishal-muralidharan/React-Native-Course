f172521dc91f1e2f938ee06f43a9958be4c06c60
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 12:04:09 +0530

    Pokemon - I

--- Pokemon/App.js
+++ Pokemon/App.js
@@ -1,16 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-    </View>
-  ) 
+    <SafeAreaView style={styles.container}>
+      <PokemonCard />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
 });
---
+++ Pokemon/components/PokemonCard.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet, Platform } from "react-native";
+
+export default function PokemonCard() {
+  return (
+    <View>
+      <Text>Pokemon Card</Text>
+    </View>
+  );
+}
