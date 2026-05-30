c306b0ff0eda9ab045ff0c052a663db93a74e93e
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 09:56:10 +0530

    flexDirection tag

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -4,13 +4,13 @@ import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f", flex: 3 }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box> */}
     </View>
   );
 }
@@ -18,6 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row-reverse",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
