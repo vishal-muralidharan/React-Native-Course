2c79fb9d509679566d88914d69474ecf95431937
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 15:42:39 +0530

    Screens and Navigation

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
