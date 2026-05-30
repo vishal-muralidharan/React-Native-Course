d1b0383cfb7b63c5fcf090d329d629df111b506b
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 13:18:06 +0530

    TextInput - II

--- RNForm/App.js
+++ RNForm/App.js
@@ -1,9 +1,13 @@
-import { StyleSheet, View, StatusBar, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, View, StatusBar, SafeAreaView, TextInput, Text } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState('');
+
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <Text style={styles.text}>My name is {name}</Text>
     </SafeAreaView>
   );
 }
@@ -19,5 +23,9 @@ const styles = StyleSheet.create({
     margin: 12,
     padding: 10,
     borderWidth: 1,
+  },
+  text: {
+    fontSize: 30,
+    padding: 10,
   },
 });
