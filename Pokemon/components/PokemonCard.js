6a1182e80b831a3e721b0519679ec243e2de045d
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 12:13:09 +0530

    Pokemon - III

--- Pokemon/components/PokemonCard.js
+++ Pokemon/components/PokemonCard.js
@@ -1,9 +1,33 @@
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, Image } from "react-native";
 
-export default function PokemonCard() {
+export default function PokemonCard({
+  name,
+  image,
+  type,
+  hp,
+  moves,
+  weaknesses,
+}) {
   return (
     <View style={styles.card}>
-      <Text>Pokemon Card</Text>
+      <View>
+        <Text>{name}</Text>
+        <Text>{hp}</Text>
+      </View>
+
+      <Image source={image} accessibilityLabel={`${name} pokemon`} />
+
+      <View>
+        <Text>{type}</Text>
+      </View>
+
+      <View>
+        <Text>Moves: {moves.join(", ")}</Text>
+      </View>
+
+      <View>
+        <Text>Weakness: {weaknesses.join(", ")}</Text>
+      </View>
     </View>
   );
 }
