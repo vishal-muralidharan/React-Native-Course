3e0843f764834d9ff01d3d583786416d21fcb703
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 11:52:55 +0530

    Platform - II

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,4 +1,5 @@
 import { SafeAreaView, View, Text, StyleSheet, Platform } from "react-native";
+import CustomButton from "./components/CustomButton/CustomButton";
 
 export default function App() {
   return (
@@ -6,6 +7,7 @@ export default function App() {
       <View style={styles.container}>
         <View style={styles.box}>
           <Text style={styles.text}>Welcome</Text>
+          <CustomButton title="Press me" onPress={() => alert("Pressed!")} />
         </View>
       </View>
     </SafeAreaView>
