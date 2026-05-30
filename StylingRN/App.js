f24f22206b2cba7cdfdbb7bdb77301edaf01f698
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 09:14:36 +0530

    Stylesheet API

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Text>StyleSheets API</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
 });
