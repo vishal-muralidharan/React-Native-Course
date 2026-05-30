c05288e41e423fd5e0ab42899366f49175ecce39
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 17:44:02 +0530

    tabBar Styling

--- RNNavigation2/App.js
+++ RNNavigation2/App.js
@@ -1,16 +1,28 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator()
+// Import your screens from the screens folder
+import CourseListScreen from './screens/CourseListScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return(
+  return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen />
-        <Tab.Screen />
-        <Tab.Screen />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelPosition: "below-icon",
+          tabBarShowLabel: true,
+          tabBarActiveTintColor: "purple",
+        }}
+      >
+        <Tab.Screen name="Course List" component={CourseListScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
