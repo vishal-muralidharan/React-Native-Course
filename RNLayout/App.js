58fa23197825911ad59b7298a68c9df949a2f997
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 10:32:21 +0530

    flexShrink and flexGrow tags

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -4,24 +4,23 @@ import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>Box 1 Shrink</Box>
+      <Box style={{ backgroundColor: "#b65d1f", flexGrow: 2 }}>Box 2 Shrink</Box>
+      {/* <Box style={{ backgroundColor: "#1c4c56", flexBasis: 140, flex: 1 }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156", height: 140, flex: 1 }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexWrap: 'wrap',
-    height: 300,
-   /*  rowGap: 10,
-    columnGap: 20, */
-    gap: 30,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    width: 300,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
