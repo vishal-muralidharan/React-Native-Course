de79033ddd0526b6d85bf6f765ea620ca6c0ee38
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 17:40:16 +0530

    Tab Navigation - Setup

--- RNNavigation2/App.js
+++ RNNavigation2/App.js
@@ -1,38 +1,16 @@
-import "./polyfill";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import DashboardScreen from "./screens/DashboardScreen";
-import SettingsScreen from "./screens/SettingsScreen";
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator()
 
 export default function App() {
-  return (
+  return(
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen 
-          name="Dashboard" 
-          component={DashboardScreen} 
-          options={{
-            title: "My dashboard",
-            drawerLabel: "Dashboard label",
-            drawerActiveTintColor: "#333",
-            drawerActiveBackgroundColor: "lightblue",
-            drawerContentStyle: {
-              backgroundColor: "#c6cbef",
-            },
-          }}
-        />
-        <Drawer.Screen 
-          name="Settings" 
-          component={SettingsScreen} 
-          options={{
-            title: "App Settings", 
-            drawerLabel: "Preferences" 
-          }}
-        />
-      </Drawer.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen />
+        <Tab.Screen />
+        <Tab.Screen />
+      </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
---
+++ RNNavigation2/AppDrawer.js
@@ -0,0 +1,38 @@
+import "./polyfill";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import DashboardScreen from "./screens/DashboardScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen 
+          name="Dashboard" 
+          component={DashboardScreen} 
+          options={{
+            title: "My dashboard",
+            drawerLabel: "Dashboard label",
+            drawerActiveTintColor: "#333",
+            drawerActiveBackgroundColor: "lightblue",
+            drawerContentStyle: {
+              backgroundColor: "#c6cbef",
+            },
+          }}
+        />
+        <Drawer.Screen 
+          name="Settings" 
+          component={SettingsScreen} 
+          options={{
+            title: "App Settings", 
+            drawerLabel: "Preferences" 
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
