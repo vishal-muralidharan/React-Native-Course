c8f1fe50c553e88323462a3ad8a159d08cbc2255
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 10:02:03 +0530

    justifyContent tag

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row-reverse",
+    justifyContent: "center",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
