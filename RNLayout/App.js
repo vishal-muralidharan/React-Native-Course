a67fb2c8d70cd766ae46c82947b5365e74e24ef2
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 10:35:00 +0530

    Position tag

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -4,13 +4,13 @@ import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>Box 1 Shrink</Box>
-      <Box style={{ backgroundColor: "#b65d1f", flexGrow: 2 }}>Box 2 Shrink</Box>
-      {/* <Box style={{ backgroundColor: "#1c4c56", flexBasis: 140, flex: 1 }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156", height: 140, flex: 1 }}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803", position: 'absolute', top: 20, left: 40 }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56", position: 'relative', top: 20, left: 40 }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
     </View>
   );
 }
@@ -18,7 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
     alignItems: "flex-start",
     width: 300,
     marginTop: 64,
