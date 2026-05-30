f216769a4e6abad5d98b4c7c787a44928b2ce1f5
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 13:14:54 +0530

    TextInput - I

--- RNForm/App.js
+++ RNForm/App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar, SafeAreaView, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +12,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
   },
 });
