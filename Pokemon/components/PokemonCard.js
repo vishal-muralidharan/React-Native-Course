35fa19445acf7dbc64a4db95de1a919618c1b199
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 12:07:16 +0530

    Pokemon - II

--- Pokemon/components/PokemonCard.js
+++ Pokemon/components/PokemonCard.js
@@ -2,8 +2,29 @@ import { View, Text, StyleSheet, Platform } from "react-native";
 
 export default function PokemonCard() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>Pokemon Card</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
