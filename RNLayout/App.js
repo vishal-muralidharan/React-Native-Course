fdc5215ea50cfc7f8c57e50563998c0ddafcb455
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 10:15:40 +0530

    flexWrap tag

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -4,11 +4,11 @@ import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803", alignSelf: "auto" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
       <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
     </View>
@@ -17,8 +17,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'flex-end',
+    flexWrap: 'wrap-reverse',
     flex: 1,
+    flexDirection: 'row',
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
