c5316a2afd59651b638545198ca133e9e3f55e7b
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 12:17:46 +0530

    Pokemon - IV

--- Pokemon/components/PokemonCard.js
+++ Pokemon/components/PokemonCard.js
@@ -10,12 +10,17 @@ export default function PokemonCard({
 }) {
   return (
     <View style={styles.card}>
-      <View>
-        <Text>{name}</Text>
-        <Text>{hp}</Text>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>❤️{hp}</Text>
       </View>
 
-      <Image source={image} accessibilityLabel={`${name} pokemon`} />
+      <Image
+        source={image}
+        style={styles.image}
+        accessibilityLabel={`${name} pokemon`}
+        resizeMode="contain"
+      />
 
       <View>
         <Text>{type}</Text>
@@ -50,5 +55,22 @@ const styles = StyleSheet.create({
         elevation: 5,
       },
     }),
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
   },
 });
