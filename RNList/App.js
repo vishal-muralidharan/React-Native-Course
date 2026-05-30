6c9d6e9b37a2bed096e87f8cb639fc384af52602
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 12:47:20 +0530

    List Rendering - CSS

--- RNList/App.js
+++ RNList/App.js
@@ -1,28 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, StatusBar as RNStatusBar } from 'react-native';
 import pokemonList from './data.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {pokemonList.map((pokemon) => {
-        return (
-          <View key={pokemon.id}>
-            <Text>{pokemon.type}</Text>
-            <Text>{pokemon.name}</Text>
-          </View>
-        );
-      })}
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+          return (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.itemText}>{pokemon.type}</Text>
+              <Text style={styles.itemText}>{pokemon.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: RNStatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+  itemText: {
+    fontSize: 30,
   },
 });
