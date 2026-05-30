33298894ff9ba437cba7a65041e5a6b024c8242b
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-30 12:38:13 +0530

    List Rendering - I

--- RNList/App.js
+++ RNList/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import pokemonList from './data.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {pokemonList.map((pokemon) => {
+        return (
+          <View key={pokemon.id}>
+            <Text>{pokemon.type}</Text>
+            <Text>{pokemon.name}</Text>
+          </View>
+        );
+      })}
       <StatusBar style="auto" />
     </View>
   );
