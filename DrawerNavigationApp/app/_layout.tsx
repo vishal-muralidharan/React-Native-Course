a2aaf46e43a0d6565388aead4297ad5af2a4a9f3
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 11:58:57 +0530

    Drawer Navigation - II

--- DrawerNavigationApp/app/_layout.tsx
+++ DrawerNavigationApp/app/_layout.tsx
@@ -4,7 +4,29 @@ import { Drawer } from "expo-router/drawer";
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
+      <Drawer>
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Home",
+            title: "Overview",
+          }}
+        />
+        <Drawer.Screen
+          name="dashboard"
+          options={{
+            drawerLabel: "Dashboard",
+            title: "Dashboard",
+          }}
+        />
+        <Drawer.Screen
+          name="setting"
+          options={{
+            drawerLabel: "Settings",
+            title: "Settings",
+          }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   );
 }
