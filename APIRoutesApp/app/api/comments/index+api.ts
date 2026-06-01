db1d7cef32114af600aab198bf9d6d94ff697076
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 13:12:04 +0530

    API Routes - II

--- APIRoutesApp/app/api/comments/index+api.ts
+++ APIRoutesApp/app/api/comments/index+api.ts
@@ -3,3 +3,10 @@ import { comments } from "../../../data/comments";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(req: Request) {
+  const { text } = await req.json();
+  const newComment = { id: comments.length + 1, text };
+  comments.push(newComment);
+  return Response.json(newComment, { status: 201 });
+}
