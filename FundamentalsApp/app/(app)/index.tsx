3e48481b00c66707b6a23a3bb58efd551e883459
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 10:39:13 +0530

    Navigation - I

--- FundamentalsApp/app/(app)/index.tsx
+++ FundamentalsApp/app/(app)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 
 export default function Home() {
@@ -8,13 +8,32 @@ export default function Home() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        gap: 20
+        gap: 20,
       }}
     >
       <Text>Home page</Text>
       <Link href="/about">About</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/products">Products</Link>
+
+      <Link href="/products/best-sellers/playstation" asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Playstation</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#0ea5e9",
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
