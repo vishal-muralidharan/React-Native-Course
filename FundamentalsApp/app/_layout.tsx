b65ca16b5761ca8750d02925028a67b3e0de99e3
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 10:19:50 +0530

    Layout - Main App

--- FundamentalsApp/app/_layout.tsx
+++ FundamentalsApp/app/_layout.tsx
@@ -1,5 +1,37 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>My Expo App</Text>
+      </View>
+      <Slot />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>© Victopia</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  header: {
+    backgroundColor: "red",
+    padding: 20,
+  },
+  headerText: {
+    color: "white",
+  },
+  footer: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+  footerText: {
+    color: "white",
+  },
+});
