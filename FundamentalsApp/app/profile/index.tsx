0f1367162f4b4984a4d6437535761fd4b5e17a34
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 10:50:11 +0530

    Navigation - III

--- FundamentalsApp/app/profile/index.tsx
+++ FundamentalsApp/app/profile/index.tsx
@@ -1,7 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Redirect } from "expo-router";
 
 export default function Profile() {
+  const isLoggedIn = false;
+
+  if (!isLoggedIn) {
+    return <Redirect href="/login" />;
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Profile</Text>
