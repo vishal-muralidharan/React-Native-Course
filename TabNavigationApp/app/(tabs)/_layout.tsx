be8e43178a7c74b8a36efeaa133456c247bcbf5d
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 11:43:30 +0530

    Tab Navigation - II

--- TabNavigationApp/app/(tabs)/_layout.tsx
+++ TabNavigationApp/app/(tabs)/_layout.tsx
@@ -1,51 +1,39 @@
 import { Tabs } from "expo-router";
-import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#0ea5e9",
-        tabBarInactiveTintColor: "gray",
+        tabBarLabelPosition: "below-icon",
+        tabBarShowLabel: true,
+        headerTitle: "Recipes App",
+        tabBarActiveTintColor: "#22d3ee",
+        tabBarInactiveTintColor: "#94a3b8",
+        tabBarStyle: {
+          backgroundColor: "#0f172a",
+        },
+        headerStyle: {
+          backgroundColor: "#0f172a",
+        },
+        headerTintColor: "#f8fafc",
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons 
-              name={focused ? "home" : "home-outline"} 
-              size={24} 
-              color={color} 
-            />
-          ),
+          tabBarLabel: "Home",
         }}
       />
       <Tabs.Screen
         name="about"
         options={{
-          title: "About",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons 
-              name={focused ? "search" : "search-outline"} 
-              size={24} 
-              color={color} 
-            />
-          ),
+          tabBarLabel: "About",
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: "Profile",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons 
-              name={focused ? "person" : "person-outline"} 
-              size={24} 
-              color={color} 
-            />
-          ),
+          tabBarLabel: "My Profile",
         }}
       />
     </Tabs>
