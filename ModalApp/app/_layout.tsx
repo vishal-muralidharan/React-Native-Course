e4a159655b766958ab8c5afe461d26a28a0fdf01
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 12:20:38 +0530

    Modal - I

--- ModalApp/app/_layout.tsx
+++ ModalApp/app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen
+        name="modal"
+        options={{ title: "Modal", presentation: "modal" }}
+      />
+    </Stack>
+  );
 }
--- ModalApp/app/index.tsx
+++ ModalApp/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -7,9 +8,11 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 20,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/modal">Open modal</Link>
     </View>
   );
 }
