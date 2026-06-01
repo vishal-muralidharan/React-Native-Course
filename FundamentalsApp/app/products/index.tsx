7d62aba8623285831262de0ac94a30becedbc834
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 10:07:24 +0530

    Catch-all Segment

--- FundamentalsApp/app/products/index.tsx
+++ FundamentalsApp/app/products/index.tsx
@@ -9,6 +9,14 @@ export default function ProductList() {
       <Link href="/products/1">Product 1</Link>
       <Link href="/products/2">Product 2</Link>
       <Link href="/products/3">Product 3</Link>
+      
+      <Link href="/products/best-sellers/playstation-5">
+        PlayStation 5 (Best Sellers)
+      </Link>
+      <Link href="/products/deals/black-friday/playstation-5">
+        PlayStation 5 (Deals)
+      </Link>
+      <Link href="/products/search/playstation-5">PlayStation 5 (Search)</Link>
     </View>
   );
 }
