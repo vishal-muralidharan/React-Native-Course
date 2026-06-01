d75110ea58e83687391ef84ee0476a4a2523f663
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-06-01 13:13:28 +0530

    API Routes - III

---
+++ APIRoutesApp/app/api/comments/[id]+api.ts
@@ -0,0 +1,11 @@
+import { comments } from "../../../data/comments";
+
+export async function GET(_req: Request, { id }: Record<string, string>) {
+  const comment = comments.find((comment) => comment.id === parseInt(id));
+  
+  if (!comment) {
+    return Response.json({ error: "Comment not found" }, { status: 404 });
+  }
+  
+  return Response.json(comment);
+}
